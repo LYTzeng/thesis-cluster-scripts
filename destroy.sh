b2b855f293c0cec52f6a415f9846daa14531b198
oscar <oscar217b@gmail.com>
2021-05-29 18:52:57 +0000

    cluster scripts for one-node kubernetes+kubeadm

--- destroy.sh
+++ destroy.sh
@@ -1,6 +1,3 @@
-WORKER1_IP="172.30.0.52"
-WORKER2_IP="172.30.0.54"
-
 KUBEADM_RESET="sudo kubeadm reset -f"
 CLEAR_IPTABLES="sudo iptables -F ; sudo iptables -t nat -F ; sudo iptables -t mangle -F ; sudo iptables -X"
 FLUSH_ROUTE="sudo ip route flush table main && sudo systemctl restart networking"
@@ -9,6 +6,5 @@ DEL_BR="sudo ovs-vsctl del-br br-int ; sudo ovs-vsctl del-br br-local; sudo ovs-
 DEL_CNI="sudo rm /etc/cni/net.d/1-sona-net.conf"
 EXEC_ALL="${KUBEADM_RESET} ; ${CLEAR_IPTABLES} ; ${RM_KUBECONFIG} ; ${DEL_BR} ; ${FLUSH_ROUTE} ; ${DEL_CNI}"
 
-ssh -i ~/.ssh/worker1 oscar@$WORKER1_IP "eval $EXEC_ALL"
-ssh -i ~/.ssh/worker2 oscar@$WORKER2_IP "eval $EXEC_ALL"
+
 eval $EXEC_ALL 
--- init.sh
+++ init.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-MASTER_IP="172.30.0.51"
-WORKER1_IP="172.30.0.52"
-WORKER2_IP="172.30.0.54"
+MASTER_IP="172.30.0.54"
 
 RED='\033[0;31m'
 GREEN='\033[1;32m'
@@ -40,36 +38,8 @@ mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
-KUBEADM_JOIN=$( kubeadm token create --print-join-command )"--apiserver-advertise-address=$MASTER_IP"
-
-
-echo -n "joining worker1 ..."
-stderr=$(ssh -i ~/.ssh/worker1 oscar@$WORKER1_IP "eval "sudo $KUBEADM_JOIN"" 2>&1 > /dev/null)
-suc_or_fail "joining-worker1" $stderr
-
-echo -n "joining worker2 ..."
-stderr=$(ssh -i ~/.ssh/worker2 oscar@$WORKER2_IP "eval "sudo $KUBEADM_JOIN"" 2>&1 > /dev/null)
-suc_or_fail "joining-worker2" $stderr
-
-stderr=$(scp -i ~/.ssh/worker1 $HOME/.kube/config oscar@$WORKER1_IP:/home/oscar/.kube 2>&1 > /dev/null)
-suc_or_fail "copying-kubeconfig-to-worker1" $stderr
-
-stderr=$(scp -i ~/.ssh/worker2 $HOME/.kube/config oscar@$WORKER2_IP:/home/oscar/.kube 2>&1 > /dev/null)
-suc_or_fail "copying-kubeconfig-to-worker2" $stderr
-
-echo -n "modyfing worker1 kubelet node ip ..."
-stderr=$( ssh -i ~/.ssh/worker1 oscar@$WORKER1_IP "sudo sed -i "s/^Environment=KUBELET_EXTRA_ARGS=--node-ip=.*/Environment=KUBELET_EXTRA_ARGS=--node-ip=$WORKER1_IP/g" /etc/systemd/system/kubelet.service.d/10-kubeadm.conf &&
- sudo systemctl daemon-reload &&
- sudo systemctl restart kubelet" 2>&1 > /dev/null)
-suc_or_fail "modifying-worker1-kubelet-env-var" $stderr
-
-echo -n "modyfing worker2 kubelet node ip ..."
-stderr=$( ssh -i ~/.ssh/worker2 oscar@$WORKER2_IP "sudo sed -i "s/^Environment=KUBELET_EXTRA_ARGS=--node-ip=.*/Environment=KUBELET_EXTRA_ARGS=--node-ip=$WORKER2_IP/g" /etc/systemd/system/kubelet.service.d/10-kubeadm.conf &&
- sudo systemctl daemon-reload &&
- sudo systemctl restart kubelet" 2>&1 > /dev/null)
-suc_or_fail "modifying-worker2-kubelet-env-var" $stderr
-
 echo -n "modyfing master kubelet node ip ..."
 stderr=$(sudo sed -i "s/^Environment=KUBELET_EXTRA_ARGS=--node-ip=.*/Environment=KUBELET_EXTRA_ARGS=--node-ip=$MASTER_IP/g" /etc/systemd/system/kubelet.service.d/10-kubeadm.conf && sudo systemctl daemon-reload && sudo systemctl restart kubelet)
 suc_or_fail "modifying-master-kubelet-env-var" $stderr
 
+kubectl taint nodes worker2 node-role.kubernetes.io/master-
