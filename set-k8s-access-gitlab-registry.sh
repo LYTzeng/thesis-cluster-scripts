b7d9d28103477628435dea3cfc9823ee2fa2330c
oscar <oscar217b@gmail.com>
2021-06-11 17:06:49 +0000

    Add k8s secrets to access itlab container registry

--- set-k8s-access-gitlab-registry.sh
+++ set-k8s-access-gitlab-registry.sh
@@ -9,3 +9,9 @@ kubectl create secret -n kube-system docker-registry gitlab-sona-cni \
         --docker-username=oscar \
         --docker-password=2jrYPp2hxoadGH4Hdf8Z \
         --docker-email=oscar217b@gmail.com
+
+kubectl create secret -n kube-system docker-registry gitlab-onos \
+        --docker-server=http://172.30.0.2:5050 \
+        --docker-username=oscar \
+        --docker-password=NodVYxLi4-pEyE6Qsbw7 \
+        --docker-email=oscar217b@gmail.com
