8f492b2d11729e926a67d799cf4c5c4fef3636f2
oscar <oscar217b@gmail.com>
2021-06-24 13:57:01 +0000

    Change ssh ip to outbound management ip

--- sona-init.sh
+++ sona-init.sh
@@ -5,16 +5,20 @@ WORKER1_IP="172.16.0.2"
 WORKER2_IP="172.16.0.3"
 MASTER_IP="172.16.0.1"
 
+MASTER_MGMT_IP="172.30.0.240"
+WORKER1_MGMT_IP="172.30.0.241"
+WORKER2_MGMT_IP="172.30.0.242"
+
 CLEAR_IPTABLES="sudo iptables -t nat -F && sudo iptables -F && sudo iptables -X"
 eval $CLEAR_IPTABLES
-ssh -i ~/.ssh/worker1 oscar@$WORKER1_IP "eval $CLEAR_IPTABLES"
-ssh -i ~/.ssh/worker2 oscar@$WORKER2_IP "eval $CLEAR_IPTABLES"
+ssh -i ~/.ssh/worker1 oscar@$WORKER1_MGMT_IP "eval $CLEAR_IPTABLES"
+ssh -i ~/.ssh/worker2 oscar@$WORKER2_MGMT_IP "eval $CLEAR_IPTABLES"
 
 # CHANGE_OVSDB_PORT="sudo sed -i \'/set ovsdb-server \"\$DB_FILE\"/a \\        set \\\"\$\@\\\" --remote=ptcp:6650\' /usr/local/share/openvswitch/scripts/ovs-ctl && \
 # sudo /usr/local/share/openvswitch/scripts/ovs-ctl restart"
 # eval $CHANGE_OVSDB_PORT
-# ssh -i ~/.ssh/worker1 oscar@$WORKER1_IP "eval $CHANGE_OVSDB_PORT"
-# ssh -i ~/.ssh/worker2 oscar@$WORKER2_IP "eval $CHANGE_OVSDB_PORT"
+# ssh -i ~/.ssh/worker1 oscar@$WORKER1_MGMT_IP "eval $CHANGE_OVSDB_PORT"
+# ssh -i ~/.ssh/worker2 oscar@$WORKER2_MGMT_IP "eval $CHANGE_OVSDB_PORT"
 
 sudo rm -rf /root/.kube/config
 sudo mkdir -p /root/.kube
