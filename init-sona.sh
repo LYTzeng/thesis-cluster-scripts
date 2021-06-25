70e68c377dc052a9b4846b303e341f6aafd8b6a9
oscar <oscar217b@gmail.com>
2021-06-25 13:33:08 +0000

    Use yaml file to add gitlab registry secrets to k8s instead

--- init-sona.sh
+++ init-sona.sh
@@ -1,2 +1,5 @@
 ./init.sh 10.10.0.0/16 --skip-phases=addon/kube-proxy && \
 ./sona-init.sh
+kubectl apply -f gitlab_registry_secrets/gitlab-onos-sona-nightly-docker.yaml
+kubectl apply -f gitlab_registry_secrets/gitlab-onos.yaml
+kubectl apply -f gitlab_registry_secrets/gitlab-sona-cni.yaml
