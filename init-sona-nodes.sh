d7eb030e69a615dc0c6a7d0ccd179d73a5d585f3
oscar <oscar217b@gmail.com>
2021-07-03 04:43:21 +0000

    Separate k8snode init API call from sona-onos-config

---
+++ init-sona-nodes.sh
@@ -0,0 +1,2 @@
+curl -v --user onos:rocks -X GET http://127.0.0.1:8181/onos/k8snode/configure/init/all
+curl -v --user onos:rocks -X GET http://127.0.0.1:8181/onos/k8snetworking/management/sync/rules
