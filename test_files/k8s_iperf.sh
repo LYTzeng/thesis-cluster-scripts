c907a43d2a23f2e64c87344664c77f3ca9eb0ea5
oscar <oscar217b@gmail.com>
2021-07-09 15:18:01 +0000

    Modify iperf script

--- test_files/k8s_iperf.sh
+++ test_files/k8s_iperf.sh
@@ -2,10 +2,11 @@
 # Usage:
 # ./k8s_iperf.sh ip-address
 
-iperf_log_file=~/iperf3_log/$(date +%s)baremetal_t600_A0-7_w256k_l128k.log
+iperf_log_file=~/iperf3_log/$(date +%s)k8s_pod_to_pod_t600_A0-7_w256k_l128k
 touch $iperf_log_file
 ip=$1
 for i in {1..10};do
-	# iperf3 -c $1 -p 5201 -f k -t 600 -w 512k | tail -4 | head -1
-	iperf3 -c $1 -p 5201 -f k -t 10 -w 512k  #--logfile $iperf_log_file
+	iperf3 -c $1 -p 5201 -f k -t 600 -w 512k | tail -4 | head -1 > $iperf_log_file'-'$i'.log'
+	sleep 30
+	# iperf3 -c $1 -p 5201 -f k -t 10 -w 512k  --logfile $iperf_log_file
 done
