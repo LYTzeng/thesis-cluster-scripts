5c9a40f6b2fa4785f3f4c2a1c0cd9202cd2da1ff
oscar <oscar217b@gmail.com>
2021-07-10 14:44:45 +0000

    Add CPU logging script

---
+++ test_files/log_cpu_usage_pid.sh
@@ -0,0 +1,3 @@
+PID=$1
+mkdir -p ~/test_files/log
+while true;do top -p 30074 -b -n1 |tail -1| awk '{print strftime("%Y-%m-%d-%H:%M:%S", systime()), $9}' >> ~/test_files/log/cpu_usage.log;sleep 3;done
