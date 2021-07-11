7dfeb779e56f536de54f99dd29eb778a878cf016
oscar <oscar217b@gmail.com>
2021-07-11 04:42:37 +0000

    curl scripts: Skip first test to avoid tcp slow start

--- test_files/run_all_curl_test.sh
+++ test_files/run_all_curl_test.sh
@@ -4,17 +4,17 @@ test_files=("32B" "64B" "128B" "256B" "512B" "1KB" "2KB" "4KB" "8KB" "16KB" "32K
 for e in ${test_files[@]};do
 	echo "Testing filesize: $e"
 	results=()
-	for n in {1..5};do
+	for n in {1..6};do
 		# echo "n=$n"
 		results[n]=$(./curl-pN.sh curl_test_list/$e 8)
 	done
 	echo "Avg. TTFB"
-	for n in {1..5};do
+	for n in {2..6};do
 		IFS=', ' read -r -a array <<< ${results[n]}
 		echo ${array[0]}
 	done
 	echo "Avg. JCT"
-	for n in {1..5};do
+	for n in {2..6};do
 		IFS=', ' read -r -a array <<< ${results[n]}
 		echo ${array[1]}
 	done
