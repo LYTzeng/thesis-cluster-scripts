c90a6ddde666a8a34baa3a1020230513355797ab
oscar <oscar217b@gmail.com>
2021-07-11 06:40:03 +0000

    modify baremetal ping script

--- test_files/iperf_baremetal.sh
+++ test_files/iperf_baremetal.sh
@@ -1,6 +1,7 @@
-# iperf_log_file=~/iperf3_log/$(date +%s)baremetal_t600_A0-7_w256k_l128k.log
-# touch $iperf_log_file
+mkdir -p ~/iperf3_log/
+extra_args=$2
+iperf_log_file=~/iperf3_log/$(date +%s)baremetal$2_t600_A0-7_w256k_l128k
 for i in {1..10};do
-	iperf3 -c 192.168.60.2 -p 5201 -f k -t 600 -w 512k | tail -4 | head -1
-	# iperf3 -c 192.168.60.2 -p 5201 -f k -t 10 -w 512k  #--logfile $iperf_log_file
+	iperf3 -c $1 -p 5201 -f k -t 610 -O 10 | tail -4 | head -1 > $iperf_log_file'-'$i'.log'
+	sleep 30
 done
