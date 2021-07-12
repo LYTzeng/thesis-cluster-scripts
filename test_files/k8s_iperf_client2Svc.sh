ef8a8cfddffb0001c7a88974d4ee2967bd32bb1b
oscar <oscar217b@gmail.com>
2021-07-12 11:16:01 +0000

    Add udp to iperf script

--- test_files/k8s_iperf_client2Svc.sh
+++ test_files/k8s_iperf_client2Svc.sh
@@ -5,9 +5,10 @@
 ip=$1
 iperf_port=$2
 sona_env=$3
+additional_flags=$4
 mkdir -p $HOME/test_files/log
 iperf_log_file=$HOME/test_files/log/$(date +%s)_$3_client_to_k8s_svc_iperf_t600_A0-7_w256k_l128k
 for i in {1..10};do
-	iperf3 -c $1 -p $iperf_port -f k -t 610 -O 10 | tail -4 | head -1 > $iperf_log_file'-'$i'.log'
+	iperf3 -c $1 $additional_flags -p $iperf_port -f k -t 610 -O 10 | tail -4 | head -1 > $iperf_log_file'-'$i'.log'
 	sleep 30
 done
